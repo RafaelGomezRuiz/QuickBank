67d9276ef9255924b302292629b9a0cee6cf5a07
01010011011001010110010001110100 <moisesgironarias@gmail.com>
2024-07-02 17:52:38 -0400

    Adding the neccesary sources to the Domain layer

---
+++ QuickBank.Core.Domain/Commons/AuditableBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickBank.Core.Domain.Commons
+{
+    public class AuditableBaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public string? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedTime { get; set; }
+    }
+}
