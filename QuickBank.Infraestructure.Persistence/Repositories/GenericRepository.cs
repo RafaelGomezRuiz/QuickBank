a6c229583508e53b151227edfd2bc8677d502f53
Rafael Gomez <rafaelogomezr@gmail.com>
2024-07-02 17:10:30 -0400

    Cada proyecto de application y de web app tienen sus paquetes y estan relacionados con los demas proyectos

---
+++ QuickBank.Infraestructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace QuickBank.Infraestructure.Persistence.Repositories
+{
+    public class GenericRepository<Entity> : IGenericRepository<Entity> where Entity : class
+    {
+        private readonly ApplicationContext _dbContext;
+        public GenericRepository(ApplicationContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+        }
+        public virtual async Task<Entity> AddAsync(Entity entity)
+        {
+            await _dbContext.Set<Entity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task DeleteAsync(Entity entity)
+        {
+            _dbContext.Set<Entity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<IEnumerable<Entity>> GetAllAsync()
+        {
+            return await _dbContext.Set<Entity>().ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<Entity>> GetAllWithIncludeAsync(IEnumerable<string> properties)
+        {
+            var query = _dbContext.Set<Entity>().AsQueryable();
+            foreach (var property in properties)
+            {
+                query = query.Include(property);
+            }
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<Entity> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Entity>().FindAsync(id);
+        }
+
+        public virtual async Task UpdateAsync(Entity entity, int id)
+        {
+            Entity entry = await _dbContext.Set<Entity>().FindAsync(id);
+            _dbContext.Entry(entry).CurrentValues.SetValues(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
