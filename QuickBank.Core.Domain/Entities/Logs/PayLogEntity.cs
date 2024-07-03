77059c4f52440858cb837934097d8644da053557
Hendry <hpeguerovaldez@gmail.com>
2024-07-02 23:40:18 -0400

    Agregando entitis de logs

---
+++ QuickBank.Core.Domain/Entities/Logs/PayLogEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace QuickBank.Core.Domain.Entities.Logs
+{
+    public class PayLogEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ QuickBank.Core.Domain/Entities/Logs/TransferLogEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace QuickBank.Core.Domain.Entities.Logs
+{
+    public class TransferLogEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate  { get; set; }
+    }
+}
