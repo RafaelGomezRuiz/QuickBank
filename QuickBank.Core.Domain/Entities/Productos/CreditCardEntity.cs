952faf447230b704c4c1d2c2ebe42c4801b736a1
01010011011001010110010001110100 <moisesgironarias@gmail.com>
2024-07-03 00:50:47 -0400

    Move to the botton the atributte UserId in CreditCardEntity

--- QuickBank.Core.Domain/Entities/Productos/CreditCardEntity.cs
+++ QuickBank.Core.Domain/Entities/Productos/CreditCardEntity.cs
@@ -4,11 +4,11 @@
     {
         public int Id { get; set; }
         public string CardNumber { get; set; }
-        public string? UserId { get; set; }
         public double Balance { get; set; }
         public double LimitCredit { get; set; }
         public int Status { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime ExpirationDate { get; set; }
+        public string? UserId { get; set; }
     }
 }
