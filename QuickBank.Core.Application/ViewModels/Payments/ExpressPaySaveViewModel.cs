8883a2bf4d998731749ef47f6038ff5187460c2c
01010011011001010110010001110100 <moisesgironarias@gmail.com>
2024-07-06 17:39:32 -0400

    Add some logic to express pay, Modified the way for show navegation bar in the header with partial views

--- QuickBank.Core.Application/ViewModels/Payments/ExpressPaySaveViewModel.cs
+++ QuickBank.Core.Application/ViewModels/Payments/ExpressPaySaveViewModel.cs
@@ -6,10 +6,12 @@ namespace QuickBank.Core.Application.ViewModels.Payments
     public class ExpressPaySaveViewModel
     {
         [Required(ErrorMessage = "The number account field cannot be empty")]
+        [StringLength(9, ErrorMessage = "The maximum number of characters is 9")]
         [DataType(DataType.Text)]
         public string NumberAccountToPay { get; set; }
 
         [Required(ErrorMessage = "The amount field cannot be empty")]
+        [Range(double.MinValue, double.MaxValue, ErrorMessage = "The number entered is not a valid amount")]
         [DataType(DataType.Currency)]
         public double Amount { get; set; }
 
--- QuickBank/Controllers/PayController.cs
+++ QuickBank/Controllers/PayController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBank.Core.Application.Helpers;
+using QuickBank.Core.Application.Interfaces.Helpers;
 using QuickBank.Core.Application.Interfaces.Services;
 using QuickBank.Core.Application.ViewModels.Payments;
 
@@ -8,20 +9,30 @@ namespace QuickBank.Controllers
     public class PayController : Controller
     {
         private readonly IPayService payService;
+        private readonly IUserHelper userHelper;
 
         public PayController(IPayService payService)
         {
             this.payService = payService;
         }
 
+        public IActionResult PayOptions()
+        {
+            return View();
+        }
+
         public IActionResult ExpressPay()
         {
-            return View(new ExpressPaySaveViewModel());
+            return View("MakeExpressPay", new ExpressPaySaveViewModel());
         }
 
         [HttpPost]
         public IActionResult ExpressPay(ExpressPaySaveViewModel epsvm)
         {
+            if (!ModelState.IsValid) return View(epsvm);
+
+            // Things
+
             return RedirectRoutesHelper.routeBasicHome;
         }
     }
